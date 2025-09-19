96fffbb8a2cf5fe8225192c44e9811c67706b134
sliitguy <it23259416@my.sliit.lk>
2025-09-19 15:37:08 +0530

    updated corrections

--- services/auth-service/src/main/java/com/sparrow/auth_service/AuthServiceApplication.java
+++ services/auth-service/src/main/java/com/sparrow/auth_service/AuthServiceApplication.java
@@ -1,10 +1,12 @@
+// services/auth-service/src/main/java/com/sparrow/auth_service/AuthServiceApplication.java
 package com.sparrow.auth_service;
-
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient  // ADD THIS ANNOTATION
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
